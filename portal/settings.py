34f681ff4233fad2698c43ea1f45a8d510b35443
Michael <31755348+michaelfeigen@users.noreply.github.com>
2018-08-01 15:37:48 -0500

    Update settings.py

--- portal/settings.py
+++ portal/settings.py
@@ -131,6 +131,6 @@ LOGIN_REDIRECT_URL = '/form/'
 #for gmail
 EMAIL_USE_TLS = True 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'carbonvfx.portal@gmail.com'
-EMAIL_HOST_PASSWORD = 'SummerFun8'
+EMAIL_HOST_USER = 'YourEmail@gmail.com'
+EMAIL_HOST_PASSWORD = 'YourPassword'
 EMAIL_PORT = 587
